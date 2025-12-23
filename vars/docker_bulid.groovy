c8d4c84a17a079abb06e588e57a50fcd9f5f762a
Tarun Sijwali <142985769+tarun583sijwali@users.noreply.github.com>
2025-12-23 21:14:55 +0530

    Update docker_bulid.groovy

--- vars/docker_bulid.groovy
+++ vars/docker_bulid.groovy
@@ -1,4 +1,12 @@
-def call(String dockerHubUser, String imageName, String imageTag = 'latest') {
-  echo "Building Docker image: ${dockerHubUser}/${imageName}:${imageTag}"
-  sh "docker build -t ${dockerHubUser}/${imageName}:${imageTag} ."
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context = config.context ?: '.'
+    
+    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
+    
+    sh """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+    """
 }
