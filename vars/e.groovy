d3a89eee180fd538d2cd95c0cdf176cceb0fbfeb
Tarun Sijwali <142985769+tarun583sijwali@users.noreply.github.com>
2025-12-26 06:03:55 +0530

    Update e.groovy

--- vars/e.groovy
+++ vars/e.groovy
@@ -1,51 +1,26 @@
-def call(String recipient,
-         String buildStatus = currentBuild.currentResult,
-         String jobName = env.JOB_NAME,
-         String buildNumber = env.BUILD_NUMBER,
-         String buildUrl = env.BUILD_URL) {
-
-    if (!recipient) {
-        error "Recipient email is required!"
-    }
-
-    def emoji = buildStatus == 'SUCCESS' 
-    def subject = "${emoji} ${buildStatus}: ${jobName} #${buildNumber}"
-
-    // Get only string data, avoid non-serializable objects
-    def user = "Unknown"
-    def cause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)
-    if (cause != null) {
-        user = cause.getUserName()
-    } else {
-        user = currentBuild.rawBuild.getCauses().collect { it.shortDescription.toString() }.join(', ')
-    }
-
-    // Email body
-    def body = buildStatus == 'SUCCESS' ? """
-        <h2>Build Succeeded: <span style="color:blue;">SUCCESS</span></h2>
-        <p>The build is successful. Great job! 🎉</p>
-        <ul>
-          <li><b>Job:</b> ${jobName}</li>
-          <li><b>Build Number:</b> ${buildNumber}</li>
-          <li><b>Build URL:</b> <a href="${buildUrl}">${buildUrl}</a></li>
-          <li><b>Triggered By:</b> ${user}</li>
-        </ul>
-    """ : """
-       <h2>Build Failed: <span style="color:red;">FAILURE</span></h2>
-        <ul>
-          <li><b>Job:</b> ${jobName}</li>
-          <li><b>Build Number:</b> ${buildNumber}</li>
-          <li><b>Build URL:</b> <a href="${buildUrl}">${buildUrl}</a></li>
-          <li><b>Console Output:</b> <a href="${buildUrl}console">${buildUrl}console</a></li>
-          <li><b>Triggered By:</b> ${user}</li>
-        </ul>
-    """
-
-    emailext(
-        to: recipient,
-        subject: subject,
-        body: body,
-        mimeType: 'text/html',
-        attachLog: buildStatus != 'SUCCESS'
-    )
-}
+// Email body
+def body = buildStatus == 'SUCCESS' ? """
+    <h2>Build Succeeded:</h2>
+    <div style="border: 2px solid blue; padding: 5px; display: inline-block; color: blue;">
+        SUCCESS
+    </div>
+    <p>The build is successful. Great job! 🎉</p>
+    <ul>
+      <li><b>Job:</b> ${jobName}</li>
+      <li><b>Build Number:</b> ${buildNumber}</li>
+      <li><b>Build URL:</b> <a href="${buildUrl}">${buildUrl}</a></li>
+      <li><b>Triggered By:</b> ${user}</li>
+    </ul>
+""" : """
+    <h2>Build Failed:</h2>
+    <div style="border: 2px solid red; padding: 5px; display: inline-block; color: red;">
+        FAILURE
+    </div>
+    <ul>
+      <li><b>Job:</b> ${jobName}</li>
+      <li><b>Build Number:</b> ${buildNumber}</li>
+      <li><b>Build URL:</b> <a href="${buildUrl}">${buildUrl}</a></li>
+      <li><b>Console Output:</b> <a href="${buildUrl}console">${buildUrl}console</a></li>
+      <li><b>Triggered By:</b> ${user}</li>
+    </ul>
+"""
